89a3db01c5863cb54ff3ed694756a5b22c3b42d0
Lujain <79986157+LujainSaad@users.noreply.github.com>
2024-07-30 15:55:43 +0300

    adding animations

--- lib/screens/meal_details.dart
+++ lib/screens/meal_details.dart
@@ -21,34 +21,47 @@ class MealDetailsScreen extends ConsumerWidget {
           title: Text(meal.title),
           actions: [
             IconButton(
-              onPressed: () {
-                final wasAdded = ref
-                    .read(favoriteMealProvider.notifier)
-                    .toggleMealFavoriteStatus(meal);
-                ScaffoldMessenger.of(context).clearSnackBars();
-                ScaffoldMessenger.of(context).showSnackBar(
-                  SnackBar(
-                    content: Text(wasAdded
-                        ? 'Meal added as a favorite.'
-                        : 'Meal removed.'),
+                onPressed: () {
+                  final wasAdded = ref
+                      .read(favoriteMealProvider.notifier)
+                      .toggleMealFavoriteStatus(meal);
+                  ScaffoldMessenger.of(context).clearSnackBars();
+                  ScaffoldMessenger.of(context).showSnackBar(
+                    SnackBar(
+                      content: Text(wasAdded
+                          ? 'Meal added as a favorite.'
+                          : 'Meal removed.'),
+                    ),
+                  );
+                },
+                icon: AnimatedSwitcher(
+                  duration: const Duration(milliseconds: 300),
+                  transitionBuilder: (child, animation) {
+                    return RotationTransition(
+                      // turns: animation,
+                      turns: Tween(begin: 0.8, end: 1.0).animate(animation),
+                      child: child,
+                    );
+                  },
+                  child: Icon(
+                    isFavorite ? Icons.star : Icons.star_border,
+                    size: 28,
+                    key: ValueKey(isFavorite),
                   ),
-                );
-              },
-              icon: Icon(
-                isFavorite ? Icons.star : Icons.star_border,
-                size: 28,
-              ),
-            )
+                ))
           ],
         ),
         body: SingleChildScrollView(
           child: Column(
             children: [
-              Image.network(
-                meal.imageUrl,
-                height: 300,
-                width: double.infinity,
-                fit: BoxFit.cover,
+              Hero(
+                tag: meal.id,
+                child: Image.network(
+                  meal.imageUrl,
+                  height: 300,
+                  width: double.infinity,
+                  fit: BoxFit.cover,
+                ),
               ),
               const SizedBox(height: 14),
               Text(
